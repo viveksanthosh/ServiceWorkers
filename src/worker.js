41b2620a60c1ba2e6f448cb8dca143a8a6b6fa45
Viveksanthosh <viveksanthosh@holidayiq.com>
2018-08-26 17:07:15 +0530

    added worker

--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,12 @@
-console.log('working')
+self.addEventListener('install', (e => {
+    console.log('instaled')
+}))
+
+self.addEventListener('activate', (e => {
+    console.log('activated')
+}))
+
+self.addEventListener('fetch', (e => {
+    console.log('fetch')
+    console.log(e)
+}))
